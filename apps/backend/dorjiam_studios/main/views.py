67fda2c1a868b2f4d111d4b592151a982c933eb7
Dorjiam <DorjiamNotKnown@gmail.com>
2024-04-28 20:55:27 +0330

    Changing the up-down animation to only apply in the home page

--- apps/backend/dorjiam_studios/main/views.py
+++ apps/backend/dorjiam_studios/main/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 
+
 def index(request):
     return render(request, 'base.html')
 
 
 def home(request):
     return render(request, 'home.html')
+
+
+def projects(request):
+    return render(request, 'projects.html')
